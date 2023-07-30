221006cdd2b1269e815a3645c6cc1b5d30b3fc18
zfdurum <zfurkandurum@gmail.com>
2023-07-30 12:24:54 +0300

    Initial Commit

---
+++ netflixClone1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflixClone1
+//
+//  Created by Zeki Furkan Durum on 30.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
